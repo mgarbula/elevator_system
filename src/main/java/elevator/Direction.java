e5eccadae424c1cebc8554f1e57087ec1c469861
mgarbula <mgarbula@student.agh.edu.pl>
2024-05-23 19:17:58 +0200

    Add Direction class

---
+++ src/main/java/elevator/Direction.java
@@ -0,0 +1,9 @@
+package elevator;
+
+public class Direction {
+
+    public static final String UP = "UP";
+    public static final String DOWN = "DOWN";
+    public static final String STOP = "STOP";
+
+}
